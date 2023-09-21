5b3cfe3b6e845f23abdf71a954e8bba70cfa1338
Pandurang1212 <pandurangbugade@gmail.com>
2023-09-21 15:14:25 +0530

    aded

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,10 +1,9 @@
-resource "aws_ami_copy" "web" {
+resource "aws_ami" "web" {
   name              = "terraform-example-web"
-  description       = "A copy of ami-web"
-  source_ami_id     = "ami-03a6eaae9938c858c"
+  description       = "ami-web"
+  ami               = "ami-03a6eaae9938c858c"
   instance_type     = "t2.micro"
-  source_ami_region = "us-west-1"
-
+  
   tags = {
     Name = "HelloWorld"
   }
